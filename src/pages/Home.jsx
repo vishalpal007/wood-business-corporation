57bd0bb2cba34d450fe8388b96e8afb8e7a4b0a2
vishal <vp461365@gmail.com>
2024-12-09 11:53:02 +0530

    updated code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,16 +16,6 @@ const Home = () => {
             src={images}
             alt="Welcome banner for the homepage"
           />
-
-          {/* "Contact Me" Button with Custom Color */}
-          {/* <button
-            onClick={handleCallClick}
-            className="absolute lg:left-[150px] md:left-[120px] md:top-[370px] lg:top-[440px] top-[200px] left-20 transform -translate-x-1/2 -translate-y-1/2 bg-[#5c483f] text-white text-sm flex items-center justify-center space-x-2 py-2 px-4 rounded-md shadow-lg hover:bg-opacity-80 transition duration-300 md:py-2 md:px-4"
-            aria-label={`Call us at ${phoneNumber}`}
-          >
-            <IoMdContacts className="text-lg mr-1" />
-            <Link to="/contact">Contact Me</Link>
-          </button> */}
         </div>
       </div>
 
